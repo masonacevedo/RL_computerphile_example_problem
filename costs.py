7a3d264c6564a41128fdfa999f7741a3d790a0fd
Mason Acevedo <mason.acevedo2000@gmail.com>
2025-08-28 10:47:18 -0700

    fix costs

--- costs.py
+++ costs.py
@@ -2,7 +2,7 @@
 from states import State
 from actions import Action
 
-def C(s: State, a: Action) -> int:
+def C(s: State, a: Action) -> State:
 
     if (s, a) == (State.HOME, Action.TAKE_BIKE):
         return 45
@@ -10,22 +10,23 @@ def C(s: State, a: Action) -> int:
     if (s, a) == (State.HOME, Action.TAKE_CAR):
         return 1
 
-    if (s, a) == (State.HOME, Action.SIT_IN_LIGHT_TRAFFIC):
+    if (s, a) == (State.LIGHT_TRAFFIC, Action.SIT_IN_LIGHT_TRAFFIC):
         return 20
-    if (s, a) == (State.HOME, Action.SIT_IN_MEDIUM_TRAFFIC):
-        return 30
-    if (s, a) == (State.HOME, Action.SIT_IN_HEAVY_TRAFFIC):
-        return 70
     
+    if (s, a) == (State.MEDIUM_TRAFFIC, Action.SIT_IN_MEDIUM_TRAFFIC):
+        return 30
+    
+    if (s, a) == (State.HEAVY_TRAFFIC, Action.SIT_IN_HEAVY_TRAFFIC):
+        return 70
 
     if (s, a) == (State.HOME, Action.TAKE_TRAIN):
         return 2
-
+    
     if (s, a) == (State.ON_TRAIN, Action.SIT_ON_TRAIN):
         return 35
 
     if (s, a) == (State.AT_STATION, Action.WAIT_FOR_TRAIN):
         return 3
-    
-    if (s, a) == (State.AT_STATION, Action.GO_BACK_HOME):
+
+    if (s, a) == {State.AT_STATION, Action.GO_BACK_HOME}:
         return 2
